8e3d9b3a4b682c15f02787132159a63a97796ce8
RAJAT MISHRA <rajatmishra@agvahealthcare.com>
2021-10-07 14:40:54 +0530

    first commit

---
+++ Suction_Pump(arduino)/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 10/7/2021 1:56:09 PM
+ * Author : agva
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
